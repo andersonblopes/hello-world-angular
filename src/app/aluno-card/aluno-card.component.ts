9ecf31b671cca976b4ea6e97f698c95ef3085406
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-08-22 20:00:57 -0300

    Binding de propriedades customizadas com @Input.

---
+++ src/app/aluno-card/aluno-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-aluno-card',
+  templateUrl: './aluno-card.component.html',
+  styleUrls: ['./aluno-card.component.css']
+})
+export class AlunoCardComponent {
+  @Input('object') aluno : any
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AlunoCardComponent } from './aluno-card/aluno-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunoCardComponent
   ],
   imports: [
     BrowserModule,
