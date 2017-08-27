452e2622cdced0465df1f3e64ab473fea871c98f
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-08-26 23:06:56 -0300

    Template-driven Forms: Criando um formulário.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AlunoCardComponent } from './aluno-card/aluno-card.component';
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { CampoColoridoDirective } from './campo-colorido.directive';
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlunoCardComponent,
     AlunoFormComponent,
-    CampoColoridoDirective
+    CampoColoridoDirective,
+    CadastroClienteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cadastro-cliente/cadastro-cliente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro-cliente',
+  templateUrl: './cadastro-cliente.component.html',
+  styleUrls: ['./cadastro-cliente.component.css']
+})
+export class CadastroClienteComponent {
+
+  salvar(form: NgForm) {
+    console.log(form);
+  }
+
+}
